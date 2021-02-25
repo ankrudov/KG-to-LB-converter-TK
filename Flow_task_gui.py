0348c6fbd119cba7be4794f861a978ecee99650a
ankrudov <lpakaco@gmail.com>
2021-02-25 06:48:51 -0600

    main tkinter file

---
+++ Flow_task_gui.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+window = tk.Tk()
+#add widget codes here
+
+#title widget
+title_main = tk.Label(text="KG to LB converter")
+title_main.pack() #adding the title to the window
+window.mainloop()
